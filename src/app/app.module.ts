0c3594533d0f81fad0a1b88f23b3bba07f20ee57
John Rowley <outsidethebox@eircom.net>
2021-05-05 21:01:36 +0100

    MovieApp:May05:2021:Night1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MovielistingComponent } from './components/movielisting/movielisting.component';
+import { MoviedetailComponent } from './components/moviedetail/moviedetail.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovielistingComponent,
+    MoviedetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
