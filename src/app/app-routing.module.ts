fb0a24c08637988b1ab1e2d8cd61e9f7b8532897
John Rowley <outsidethebox@eircom.net>
2021-05-12 20:52:05 +0100

    added booking func

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './components/booking/booking.component';
+import { MovielistingComponent } from './components/movielisting/movielisting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'booking', component: BookingComponent},
+  { path: 'movies', component: MovielistingComponent},
+  { path: '', redirectTo: 'movies', pathMatch: 'full'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
